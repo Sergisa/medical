c8381b5653d68175859cc73e920e025adf44d129
Sergisa <phebra@yandex.ru>
2022-06-21 14:12:13 +0300

    функция для тестирования

---
+++ src/js/testDriver.js
@@ -0,0 +1,11 @@
+function fillTestData() {
+    $('#erythrocytes').val(5.4);
+    $('#hemoglobin').val(124);
+    $('#pulse').val(50);
+    $('#hematocrit').val(34);
+    $('#age').val(36);
+    $('#bloodArterialPressure').val(120);
+    $('#lossConsciousness').attr('checked', true);
+    $('#anamnesisGastrointestinalUndefinedBleeding').attr('checked', true);
+    $('input#male').attr('checked', true);
+}
