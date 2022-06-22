4d055112c37768224a79fc299e6a8e0e0559a7da
Sergisa <phebra@yandex.ru>
2022-06-22 14:58:24 +0300

    стилизация

--- src/js/sequence.js
+++ src/js/sequence.js
@@ -9,7 +9,7 @@ function adviceResearch(researchName, needDataEdit) {
 }
 
 function info(message) {
-    console.info(`%c ${message}`, 'color:#4CAF50; font-weight: bolder')
+    console.info(`%c ${message} `, 'color:#2ECC71; background-color:#2C3E50; font-size:14px; font-weight: bolder')
 }
 
 function showQuestion(question) {
