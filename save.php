1c7f672366e879de6b7f701ef37d419ba407ccfc
sergisa <phebra@yandex.ru>
2022-09-06 02:04:49 +0300

    Сохранение

--- save.php
+++ save.php
@@ -1,1 +1,36 @@
 <?php
+$host = 'sergisa.ru';
+$db = 'medical';
+$user = 'user15912_sergey';
+$pass = 'isakovs';
+$charset = 'utf8';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$opt = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
+];
+$dbh = new PDO($dsn, $user, $pass, $opt);
+echo json_encode($_POST);
+
+function pdoSet($values)
+{
+    $set = [];
+    foreach ($values as $key => $value) {
+
+        if ($value == 'true') $value = 1;
+        if (empty($value)) $value = 'null';
+        $set []= "`$key`=$value";
+    }
+    return implode(',', $set);
+}
+
+$data = array_merge($_POST['data'], $_POST['conclusion']);
+$sql = "INSERT INTO patient SET " . pdoSet($data);
+
+echo "SQL" . $sql;
+$stm = $dbh->prepare($sql);
+$stm->execute();
+
+
