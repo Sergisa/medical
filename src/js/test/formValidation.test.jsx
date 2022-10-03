03296c37e18bdd83854925cf76f58330a1d5ba3c
Sergisa <phebra@yandex.ru>
2022-10-03 16:04:15 +0300

    Добавлены тесты UI

---
+++ src/js/test/formValidation.test.jsx
@@ -0,0 +1,57 @@
+global.$ = require('jquery')
+const formValidation = require('../formValidation')
+const test = require('@jest/globals').test
+const expect = require('@jest/globals').expect
+
+function prepareDOM(elements) {
+    document.body.innerHTML = "";
+    for (const element in elements) {
+        const input = document.createElement('input');
+        input.value = elements[element]
+        input.id = element
+        document.body.appendChild(input);
+    }
+}
+
+test("Десятичные значения читаются (.)", () => {
+    prepareDOM({
+        erythrocytes: "11.2",
+        hemoglobin: "12.2",
+        pulse: "13.2",
+        bloodArterialPressure: "14.2",
+        hematocrit: "15.2"
+    })
+    expect(formValidation.collectData().erythrocytes).toBe(11.2)
+    expect(formValidation.collectData().hemoglobin).toBe(12.2)
+    expect(formValidation.collectData().pulse).toBe(13.2)
+    expect(formValidation.collectData().bloodArterialPressure).toBe(14.2)
+    expect(formValidation.collectData().hematocrit).toBe(15.2)
+})
+test("Десятичные значения читаются (,)", () => {
+    prepareDOM({
+        erythrocytes: "11,2",
+        hemoglobin: "12,2",
+        pulse: "13,2",
+        bloodArterialPressure: "14,2",
+        hematocrit: "15,2"
+    })
+    expect(formValidation.collectData().erythrocytes).toBe(11.2)
+    expect(formValidation.collectData().hemoglobin).toBe(12.2)
+    expect(formValidation.collectData().pulse).toBe(13.2)
+    expect(formValidation.collectData().bloodArterialPressure).toBe(14.2)
+    expect(formValidation.collectData().hematocrit).toBe(15.2)
+})
+test("Целочисленные значения считываются", () => {
+    prepareDOM({
+        erythrocytes: "11",
+        hemoglobin: "12",
+        pulse: "13",
+        bloodArterialPressure: "14",
+        hematocrit: "15"
+    });
+    expect(formValidation.collectData().erythrocytes).toBe(11)
+    expect(formValidation.collectData().hemoglobin).toBe(12)
+    expect(formValidation.collectData().pulse).toBe(13)
+    expect(formValidation.collectData().bloodArterialPressure).toBe(14)
+    expect(formValidation.collectData().hematocrit).toBe(15)
+})
