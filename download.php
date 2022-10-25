5e3f2f02c32afef5399d0f8284a9631f2a70b537
sergisa <phebra@yandex.ru>
2022-10-26 01:42:42 +0300

    Стабильная версия скачивания

--- download.php
+++ download.php
@@ -4,80 +4,13 @@ require 'vendor/autoload.php';
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
+$database = new PDO('mysql:dbname=medical;host=sergisa.ru', 'user15912_sergey', 'isakovs');
+$patientsQuery = $database->query("SELECT * FROM patient");
+$patients = $patientsQuery->fetchAll(PDO::FETCH_ASSOC);
+$columns = array_keys($patients[0]);
 $spreadsheet = new Spreadsheet();
 $sheet = $spreadsheet->getActiveSheet();
-$sheet->setCellValue('A1', 'Hello my Friend!');
-$sheet->fromArray([
-    "ID",
-    "time",
-    "identifier",
-    "IIC",
-    "description",
-    "erythrocytes",
-    "hemoglobin",
-    "pulse",
-    "hematocrit",
-    "age",
-    "coffeeVomit",
-    "melena",
-    "lossConsciousness",
-    "bloodArterialPressure",
-    "chronicHeartFailure",
-    "cardiacIschemia",
-    "kidneyFailure",
-    "liverFailure",
-    "metastaticCancer",
-    "pathologicalChanges",
-    "aorticProsthesis",
-    "heartDefects",
-    "heartValvesProsthesis",
-    "additionalIllness",
-    "bloodVomit",
-    "hematohesia",
-    "blackFeces",
-    "collaptoidState",
-    "gastrointestinalUcler",
-    "paleSkin",
-    "hemoglobinFalls",
-    "hiddenBleeding",
-    "stickySweat",
-    "gender",
-    "anamnesisGastrointestinalUpperBleeding",
-    "anamnesisGastrointestinalUndefinedBleeding",
-    "v32",
-    "v33",
-    "v34",
-    "v35",
-    "v36",
-    "v37",
-    "v38",
-    "v39",
-    "v40",
-    "v41",
-    "v42",
-    "v43",
-    "v44",
-    "v45",
-    "v46",
-    "v47",
-    "v48",
-    "v49",
-    "ASA1",
-    "ASA2",
-    "ASA3",
-    "ASA4",
-    "ASA5",
-    "ASA6",
-    "E",
-    "explicit",
-    "bloodLossHardness",
-    "localization",
-    "finalLocalization",
-    "risk",
-    "hasResearchData",
-    "reason"
-]);
-$rowIterator = $sheet->getRowIterator(2);
+$sheet->fromArray(array_merge([$columns], $patients));
 header('Content-Type: application/vnd.ms-excel');
 header('Content-Disposition: attachment; filename="file.xlsx"');
 try {
