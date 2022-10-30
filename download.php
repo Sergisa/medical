e02285e78a73e14e81ec875869c4d8883121a8d3
sergisa <phebra@yandex.ru>
2022-10-30 23:17:24 +0300

    Добавление модального окна выбора даты

--- download.php
+++ download.php
@@ -7,6 +7,17 @@ use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 $database = new PDO('mysql:dbname=medical;host=sergisa.ru', 'user15912_sergey', 'isakovs');
 $database->exec('SET CHARACTER SET UTF8');
 $patientsQuery = $database->query("SELECT * FROM patient");
+if (
+    (array_key_exists('dateEnd', $_GET) && !empty($_GET['dateEnd'])) &&
+    (array_key_exists('dateStart', $_GET) && !empty($_GET['dateStart']))
+) {
+    $patientsQuery = $database
+        ->prepare("SELECT * FROM patient WHERE time<str_to_date(:dateEnd, '%Y-%m-%d') AND time>str_to_date(:dateStart, '%Y-%m-%d')");
+} else {
+    $_GET = [];
+}
+
+$patientsQuery->execute($_GET);
 $patients = $patientsQuery->fetchAll(PDO::FETCH_ASSOC);
 $columns = array_keys($patients[0]);
 $spreadsheet = new Spreadsheet();
@@ -19,3 +30,4 @@ try {
     $writer->save('php://output');
 } catch (\PhpOffice\PhpSpreadsheet\Writer\Exception $e) {
 }
+
